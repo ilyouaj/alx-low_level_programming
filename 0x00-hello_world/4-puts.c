8b2c6bfde9c8f3d4d0d39c41570db6263bc379f1
ilyouaj <lyouaj.ilyass@gmail.com>
2023-06-15 11:43:29 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description: A C program that print with put function
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programing is like building a multilinqual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description:A C program that print with put function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(voide)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
