1369f2674d0c3f00d5b54c6e769197e144856812
ilyouaj <lyouaj.ilyass@gmail.com>
2023-06-15 12:36:00 -0700

    tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - entry point
- *
- * Description:A C program that print with put function
- *
- * Return: Always 0 (success)
+* main - Entry point
+*
+* Return: Always 0 (Success)
 */
-
-int main(voide)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
+
