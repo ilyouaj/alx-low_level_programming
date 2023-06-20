9ddf21022633bdf55b106a02fbfdae7f8e38ebaf
ilyouaj <lyouaj.ilyass@gmail.com>
2023-06-20 14:08:18 -0700

    task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,9 @@
-#indef main_h
-# define main_h
+#ifndef  MAIN_H
+#define MAIN_H
 
-int_putchar(char);
-void  print_alphabet(void);
-void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
-int print_sign(int n);
-int_abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int,int);
-void print_to_98(int n);
-void print_times_table(int n);
+#include <stdio.h>
+
+int _putchar(char c);
 
 #endif
+
