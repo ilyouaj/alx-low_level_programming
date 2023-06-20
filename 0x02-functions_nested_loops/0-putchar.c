6c596f6ae318b7417233325d8057a641e0ff91b7
ilyouaj <lyouaj.ilyass@gmail.com>
2023-06-20 13:48:45 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
- * main - print_putchar
- * is written by mbah
- * Return: 0
+ * main - Entry point
+ *
+ * Dsecription: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char str[] = "_putchar";
+	int ch:
+	for ('ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
 	_putchar('\n');
 	return (0);
 }
