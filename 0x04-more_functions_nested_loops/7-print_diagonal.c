6618fb759051827b03ab386ebe841a293ce19db8
ilyouaj <lyouaj.ilyass@gmail.com>
2023-06-22 13:45:09 -0700

    done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,9 +17,9 @@ void print_diagonal(int n)
 	{
 		for (postn = 1; postn <= n; postn++)
 		{
-			for (space = 1; space <= n; space++)
+			for (space = 1; space < postn; space++)
 				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
+			_putchar(\\); /*is equal to '/' char*/
 			_putchar('\n');
 		}
 	}
