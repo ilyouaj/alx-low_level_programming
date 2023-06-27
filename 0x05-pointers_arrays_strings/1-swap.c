c3381c47cadf57b58ed924587a41839605d87249
ilyouaj <lyouaj.ilyass@gmail.com>
2023-06-26 17:13:36 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointers to an int parameter
+ *		and updates the value it pointer to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
