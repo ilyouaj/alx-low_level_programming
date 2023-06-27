d68316da749e81861cf5eceba226293b20206bf0
ilyouaj <lyouaj.ilyass@gmail.com>
2023-06-26 18:12:42 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,6 +15,10 @@ void rev_string(char *s)
 
 	/*find string length without null char*/
 	for (l = 0; s[l] != '\0'; ++l)
+		;
+
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < l / 2; i++)
 	{
 		temp = s[i];
 		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
