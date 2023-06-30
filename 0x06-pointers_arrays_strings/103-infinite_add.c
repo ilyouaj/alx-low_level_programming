28d422da89d7765ff38b0b9fa8273efb61f75f7c
ilyouaj <lyouaj.ilyass@gmail.com>
2023-06-29 19:47:08 -0700

    done

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -42,7 +42,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	if (d == 1)
 	{
-		r[l + 1] = '0';
+		r[l + 1] = '\0';
 		if (l + 2 > size_r)
 			return (0);
 		while (l-- >= 0)
@@ -51,5 +51,3 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	return (r)
 }
-
-
